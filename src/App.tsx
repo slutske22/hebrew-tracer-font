7526cda2f334ad2023b9b4388b189ce5809815dd
Seth Lutske <seth.lutske@gmail.com>
2023-09-28 18:34:36 -1000

    Adding more letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useRef, useState } from "react";
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 import layout from "simple-keyboard-layouts/build/layouts/hebrew";
+import ReactToPrint, { useReactToPrint } from "react-to-print";
 
 function App() {
+  const [content, setContent] = useState("");
+  const pageRef = useRef<HTMLDivElement>(null);
+
+  const handlePrint = useReactToPrint({
+    content: () => pageRef.current,
+  });
+
   return (
     <>
-      <Keyboard layout={layout.layout} />
+      <div id="page-view" ref={pageRef}>
+        {content}
+      </div>
+      <Keyboard layout={layout.layout} onChange={(e) => setContent(e)} />
+
+      <ReactToPrint content={() => pageRef.current} />
+
+      <button onClick={handlePrint}>Print!</button>
     </>
   );
 }
