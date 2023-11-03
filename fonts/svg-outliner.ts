ecea791e31c45b2e5ff06979895227122976cf97
Seth Lutske <seth.lutske@gmail.com>
2023-11-02 18:06:28 -1000

    Some more node commands to write files

--- fonts/svg-outliner.ts
+++ fonts/svg-outliner.ts
@@ -21,21 +21,29 @@ function main() {
   }
 
   if (!dest) {
-    console.error(
-      "Must provide destination directory!\n\nTry something like:\n`npm run outliner -- -s ./sourcedir -d './output`\n\n"
-    );
-    return;
+    console.warn("No destination provided, writing to source directory\n\n");
   }
 
   const files = fs.readdirSync(source);
 
-  for (const file of files) {
+  for (const file of [files[0]]) {
     if (path.extname(file) === ".svg") {
-      console.log(file);
-
       const text = fs.readFileSync(path.join(source, file), "utf-8");
 
-      console.log(text);
+      // process the text
+
+      if (dest && !fs.existsSync(dest)) {
+        console.error(`Destination directory "${dest}" does not exist\n`);
+        return;
+      }
+
+      if (!dest && !fs.existsSync(path.join(source, "output"))) {
+        fs.mkdirSync(path.join(source, "output"));
+      }
+
+      const outputPath = dest ?? path.join(source, "output");
+
+      fs.writeFileSync(path.join(outputPath, file), text);
     }
   }
 }
